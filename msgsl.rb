03d98045316b34758297b41d740edff65f5a3e54
GitHub Action <github-action@users.noreply.github.com>
2020-04-20 23:06:51 +0000

    Add bottle DSL

--- msgsl.rb
+++ msgsl.rb
@@ -7,7 +7,7 @@ class Msgsl < Formula
   depends_on "cmake" => :build
 
   bottle do
-    root_url "https://github.com/ncihnegn/homebrew-formulae/releases/download/msgsl-2.1.0"
+    root_url "https://github.com/ncihnegn/homebrew-formulae/releases/download/msgsl-3.0.0" cellar :any_skip_relocation sha256 "2720e8011430ce7f62f817d5edd45a2cb4a08eda3bfa5bed06c97ed0a15146cb" => :catalina 
   end
 
   def install
