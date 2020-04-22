8ea65c902e144ffcc250ce0aa5d1d5b98a0bf081
ncihnegn <ncihnegn@users.noreply.github.com>
2020-04-22 14:41:32 -0700

    Reduce llvm build time to get around token timeout in github actions

--- llvm9.rb
+++ llvm9.rb
@@ -9,45 +9,45 @@ class Llvm9 < Formula
     url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/llvm-#{version}.src.tar.xz"
     sha256 "00a1ee1f389f81e9979f3a640a01c431b3021de0d42278f6508391a2f0b81c9a"
 
-    resource "clang" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/clang-#{version}.src.tar.xz"
-      sha256 "5778512b2e065c204010f88777d44b95250671103e434f9dc7363ab2e3804253"
-    end
+    #resource "clang" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/clang-#{version}.src.tar.xz"
+    #  sha256 "5778512b2e065c204010f88777d44b95250671103e434f9dc7363ab2e3804253"
+    #end
 
-    resource "clang-tools-extra" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/clang-tools-extra-#{version}.src.tar.xz"
-      sha256 "b26fd72a78bd7db998a26270ec9ec6a01346651d88fa87b4b323e13049fb6f07"
-    end
+    #resource "clang-tools-extra" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/clang-tools-extra-#{version}.src.tar.xz"
+    #  sha256 "b26fd72a78bd7db998a26270ec9ec6a01346651d88fa87b4b323e13049fb6f07"
+    #end
 
     resource "compiler-rt" do
       url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/compiler-rt-#{version}.src.tar.xz"
       sha256 "c2bfab95c9986318318363d7f371a85a95e333bc0b34fbfa52edbd3f5e3a9077"
     end
 
-    resource "polly" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/polly-#{version}.src.tar.xz"
-      sha256 "9a4ac69df923230d13eb6cd0d03f605499f6a854b1dc96a9b72c4eb075040fcf"
-    end
+    #resource "polly" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/polly-#{version}.src.tar.xz"
+    #  sha256 "9a4ac69df923230d13eb6cd0d03f605499f6a854b1dc96a9b72c4eb075040fcf"
+    #end
 
-    resource "lld" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/lld-#{version}.src.tar.xz"
-      sha256 "86262bad3e2fd784ba8c5e2158d7aa36f12b85f2515e95bc81d65d75bb9b0c82"
-    end
+    #resource "lld" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/lld-#{version}.src.tar.xz"
+    #  sha256 "86262bad3e2fd784ba8c5e2158d7aa36f12b85f2515e95bc81d65d75bb9b0c82"
+    #end
 
-    resource "openmp" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/openmp-#{version}.src.tar.xz"
-      sha256 "5c94060f846f965698574d9ce22975c0e9f04c9b14088c3af5f03870af75cace"
-    end
+    #resource "openmp" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/openmp-#{version}.src.tar.xz"
+    #  sha256 "5c94060f846f965698574d9ce22975c0e9f04c9b14088c3af5f03870af75cace"
+    #end
 
     resource "libcxx" do
       url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/libcxx-#{version}.src.tar.xz"
       sha256 "0981ff11b862f4f179a13576ab0a2f5530f46bd3b6b4a90f568ccc6a62914b34"
     end
 
-    resource "libunwind" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/libunwind-#{version}.src.tar.xz"
-      sha256 "535a106a700889274cc7b2f610b2dcb8fc4b0ea597c3208602d7d037141460f1"
-    end
+    #resource "libunwind" do
+    #  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-#{version}/libunwind-#{version}.src.tar.xz"
+    #  sha256 "535a106a700889274cc7b2f610b2dcb8fc4b0ea597c3208602d7d037141460f1"
+    #end
   end
 
   bottle do
@@ -111,41 +111,42 @@ class Llvm9 < Formula
     # almost be treated as an entirely different build from LLVM.
     ENV.permit_arch_flags
 
-    clang_buildpath  = buildpath/"tools/clang"
+    #clang_buildpath  = buildpath/"tools/clang"
     libcxx_buildpath = buildpath/"projects/libcxx"
 
-    clang_buildpath.install resource("clang")
+    #clang_buildpath.install resource("clang")
     libcxx_buildpath.install resource("libcxx")
-    (buildpath/"tools/lld").install resource("lld")
-    (buildpath/"tools/polly").install resource("polly")
-    (buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
-    (buildpath/"projects/openmp").install resource("openmp")
-    (buildpath/"projects/libunwind").install resource("libunwind")
+    #(buildpath/"tools/lld").install resource("lld")
+    #(buildpath/"tools/polly").install resource("polly")
+    #(buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
+    #(buildpath/"projects/openmp").install resource("openmp")
+    #(buildpath/"projects/libunwind").install resource("libunwind")
     (buildpath/"projects/compiler-rt").install resource("compiler-rt")
 
     install_prefix = lib/"llvm-#{ver}"
 
     args = %W[
-      -DCMAKE_INSTALL_PREFIX=#{install_prefix}
       -DCMAKE_BUILD_TYPE=Release
-      -DLLVM_ENABLE_ASSERTIONS=ON
-      -DLLVM_OPTIMIZED_TABLEGEN=ON
-      -DLLVM_INCLUDE_DOCS=OFF
-      -DLLVM_ENABLE_RTTI=ON
-      -DLLVM_ENABLE_EH=ON
-      -DLLVM_INSTALL_UTILS=ON
-      -DWITH_POLLY=ON
-      -DLINK_POLLY_INTO_TOOLS=ON
-      -DLLVM_TARGETS_TO_BUILD=all
+      -DCMAKE_INSTALL_PREFIX=#{install_prefix}
+      -DFFI_INCLUDE_DIR=#{Formula["libffi"].opt_lib}/libffi-#{Formula["libffi"].version}/include
+      -DFFI_LIBRARY_DIR=#{Formula["libffi"].opt_lib}
       -DLIBOMP_ARCH=x86_64
       -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON
       -DLLVM_BUILD_LLVM_DYLIB=ON
-      -DLLVM_LINK_LLVM_DYLIB=ON
+      -DLLVM_CREATE_XCODE_TOOLCHAIN=OFF
+      -DLLVM_ENABLE_ASSERTIONS=ON
+      -DLLVM_ENABLE_EH=ON
       -DLLVM_ENABLE_LIBCXX=ON
       -DLLVM_ENABLE_FFI=ON
-      -DFFI_INCLUDE_DIR=#{Formula["libffi"].opt_lib}/libffi-#{Formula["libffi"].version}/include
-      -DFFI_LIBRARY_DIR=#{Formula["libffi"].opt_lib}
-      -DLLVM_CREATE_XCODE_TOOLCHAIN=ON
+      -DLLVM_ENABLE_RTTI=ON
+      -DLLVM_INCLUDE_DOCS=OFF
+      -DLLVM_INSTALL_UTILS=ON
+      -DLLVM_LINK_LLVM_DYLIB=ON
+      -DLLVM_OPTIMIZED_TABLEGEN=ON
+      -DLLVM_PARALLEL_COMPILE_JOBS=2
+      -DWITH_POLLY=OFF
+      -DLINK_POLLY_INTO_TOOLS=OFF
+      -DLLVM_TARGETS_TO_BUILD="X86"
     ]
 
     mkdir "build" do
@@ -155,18 +156,18 @@ class Llvm9 < Formula
       system "make", "install-xcode-toolchain"
     end
 
-    (share/"clang-#{ver}/tools").install Dir["tools/clang/tools/scan-{build,view}"]
-    inreplace share/"clang-#{ver}/tools/scan-build/bin/scan-build", "$RealBin/bin/clang", install_prefix/"bin/clang"
-    (install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-view/bin/scan-view"
-    (install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-build/bin/scan-build"
-    (install_prefix/"share/man/man1").install_symlink share/"clang-#{ver}/tools/scan-build/scan-build.1"
+    #(share/"clang-#{ver}/tools").install Dir["tools/clang/tools/scan-{build,view}"]
+    #inreplace share/"clang-#{ver}/tools/scan-build/bin/scan-build", "$RealBin/bin/clang", install_prefix/"bin/clang"
+    #(install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-view/bin/scan-view"
+    #(install_prefix/"bin").install_symlink share/"clang-#{ver}/tools/scan-build/bin/scan-build"
+    #(install_prefix/"share/man/man1").install_symlink share/"clang-#{ver}/tools/scan-build/scan-build.1"
 
-    (lib/"python2.7/site-packages").install "bindings/python/llvm" => "llvm-#{ver}",
-                                            clang_buildpath/"bindings/python/clang" => "clang-#{ver}"
+    #(lib/"python2.7/site-packages").install "bindings/python/llvm" => "llvm-#{ver}",
+    #                                        clang_buildpath/"bindings/python/clang" => "clang-#{ver}"
 
     # replace the existing "clang -> clang-9" symlink
-    rm install_prefix/"bin/clang"
-    mv install_prefix/"bin/clang-9", install_prefix/"bin/clang"
+    #rm install_prefix/"bin/clang"
+    #mv install_prefix/"bin/clang-9", install_prefix/"bin/clang"
 
     # These versioned .dylib symlinks are missing for some reason
     # Note that we use relative symlinks
@@ -180,9 +181,9 @@ class Llvm9 < Formula
     system "install_name_tool", "-id", install_prefix/"lib/libLLVM.dylib", install_prefix/"lib/libLLVM.dylib"
     system "install_name_tool", "-id", install_prefix/"lib/libLTO.dylib", install_prefix/"lib/libLTO.dylib"
     system "install_name_tool", "-id", install_prefix/"lib/libc++.1.0.dylib", install_prefix/"lib/libc++.1.0.dylib"
-    system "install_name_tool", "-id", install_prefix/"lib/libclang.dylib", install_prefix/"lib/libclang.dylib"
-    system "install_name_tool", "-id", install_prefix/"lib/libomp.dylib", install_prefix/"lib/libomp.dylib"
-    system "install_name_tool", "-id", install_prefix/"lib/libunwind.1.0.dylib", install_prefix/"lib/libunwind.1.0.dylib"
+    #system "install_name_tool", "-id", install_prefix/"lib/libclang.dylib", install_prefix/"lib/libclang.dylib"
+    #system "install_name_tool", "-id", install_prefix/"lib/libomp.dylib", install_prefix/"lib/libomp.dylib"
+    #system "install_name_tool", "-id", install_prefix/"lib/libunwind.1.0.dylib", install_prefix/"lib/libunwind.1.0.dylib"
 
     Dir.glob(install_prefix/"bin/*") do |exec_path|
       basename = File.basename(exec_path)
