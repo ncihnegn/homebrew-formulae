0529047260b7741c9f4a15694bf5a56d4e4c75c3
ncihnegn <ncihnegn@users.noreply.github.com>
2020-05-03 22:43:09 -0700

    Add checksum

--- Casks/lx-music-desktop.rb
+++ Casks/lx-music-desktop.rb
@@ -1,5 +1,6 @@
 cask 'lx-music-desktop' do
   version '0.18.2'
+  sha256 '02f8135147320d30f19689d0797a37183bd86d33cef1296fe59fa9886c1f060e'
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg",
       user_agent: :fake
