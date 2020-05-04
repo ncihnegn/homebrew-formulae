532b7e95aa24eefb59c00a9d66a85410eebd74a5
ncihnegn <ncihnegn@users.noreply.github.com>
2020-05-03 22:42:09 -0700

    Fix

--- Casks/lx-music-desktop.rb
+++ Casks/lx-music-desktop.rb
@@ -1,7 +1,7 @@
 cask 'lx-music-desktop' do
   version '0.18.2'
 
-  url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg"
+  url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg",
       user_agent: :fake
   homepage 'https://github.com/lyswhut/lx-music-desktop'
 
