2b2bfd5f44c1920cb3df84dbb53b7ffd955deae6
ncihnegn <ncihnegn@users.noreply.github.com>
2020-09-22 22:16:13 -0700

    1.1.1

--- Casks/lx-music-desktop.rb
+++ Casks/lx-music-desktop.rb
@@ -1,6 +1,5 @@
 cask 'lx-music-desktop' do
-  version '1.0.1'
-  sha256 "bdca5b36ac11e70cbdd55c58bc6bb4d51b472959bd198839fc9f504110559b5e"
+  version '1.1.1'
 
   url "https://github.com/lyswhut/lx-music-desktop/releases/download/v#{version}/lx-music-desktop-#{version}.dmg",
       user_agent: :fake
