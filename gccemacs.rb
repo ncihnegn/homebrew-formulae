874ca64f21eb3459c413388904a75eb84bbf4b76
ncihnegn <ncihnegn@users.noreply.github.com>
2020-10-24 17:58:40 -0700

    Add gccemacs

---
+++ gccemacs.rb
@@ -0,0 +1,100 @@
+class Gccemacs < Formula
+  desc "GNU Emacs text editor"
+  homepage "https://www.gnu.org/software/emacs/"
+  url "https://github.com/ncihnegn/emacs/archive/28.0.50.201024.tar.gz"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+  end
+
+  bottle do
+  end
+
+  head do
+    url "https://github.com/emacs-mirror/emacs.git"
+
+    depends_on "autoconf" => :build
+    depends_on "gnu-sed" => :build
+    depends_on "texinfo" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gnutls"
+  depends_on "jansson"
+  depends_on "libgccjit"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+
+  on_linux do
+    depends_on "jpeg"
+  end
+
+  def install
+    args = %W[
+      --disable-silent-rules
+      --enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp
+      --infodir=#{info}/emacs
+      --prefix=#{prefix}
+      --with-gnutls
+      --without-x
+      --with-xml2
+      --without-dbus
+      --with-modules
+      --without-ns
+      --without-imagemagick
+      --with-nativecomp
+    ]
+
+    if build.head?
+      ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+      system "./autogen.sh"
+    end
+
+    File.write "lisp/site-load.el", <<~EOS
+      (setq exec-path (delete nil
+        (mapcar
+          (lambda (elt)
+            (unless (string-match-p "Homebrew/shims" elt) elt))
+          exec-path)))
+    EOS
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    # Follow MacPorts and don't install ctags from Emacs. This allows Vim
+    # and Emacs and ctags to play together without violence.
+    (bin/"ctags").unlink
+    (man1/"ctags.1.gz").unlink
+  end
+
+  plist_options manual: "emacs"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/emacs</string>
+          <string>--fg-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    assert_equal "4", shell_output("#{bin}/emacs --batch --eval=\"(print (+ 2 2))\"").strip
+  end
+end
