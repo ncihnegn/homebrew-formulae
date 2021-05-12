595f0c25d1fffbec2923aaad9e197a81a0392cda
ncihnegn <ncihnegn@users.noreply.github.com>
2021-05-12 01:42:15 -0700

    Add cask rehex

---
+++ Casks/rehex.rb
@@ -0,0 +1,8 @@
+cask 'rehex' do
+  name "REHex Engineers' Hex Editor"
+  version '0.3.91'
+  url "https://github.com/solemnwarning/rehex/releases/download/#{version}/rehex-#{version}-mac.dmg"
+  homepage 'https://github.com/solemnwarning/rehex'
+
+  app 'REHex.app'
+end
