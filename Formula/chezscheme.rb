9aa7bd261615c94c02fd2acd788723446089c1a8
ncihnegn <ncihnegn@users.noreply.github.com>
2021-06-17 20:42:12 -0700

    Add arm64 build

--- Formula/chezscheme.rb
+++ Formula/chezscheme.rb
@@ -2,10 +2,9 @@ class Chezscheme < Formula
   desc "Implementation of the Chez Scheme language"
   homepage "https://cisco.github.io/ChezScheme/"
   url "https://github.com/cisco/ChezScheme/archive/v9.5.4.tar.gz"
+  head "https://github.com/racket/ChezScheme.git"
 
-  sha256 "258a4b5284bb13ac6e8b56acf89a7ab9e8726a90cc57ea1cd71c5da442323840"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     root_url "https://github.com/ncihnegn/homebrew-formulae/releases/download/chezscheme-9.5.4"
@@ -25,10 +24,18 @@ class Chezscheme < Formula
       end
     end
 
+    if Hardware::CPU.arm?
+      system "./configure --pb"
+      system "make tarm64osx.bootquick"
+      system "mkdir", "-p", "bin/tarm64osx"
+      system "ln", "-s", "tarm64osx/bin/scheme", "bin/tarm64osx"
+    end
+
     system "./configure",
               "--installprefix=#{prefix}",
               "--threads",
               "--installschemename=chez"
+    system "make"
     system "make", "install"
   end
 
